21e4cde3a487e6f6f17d0d8cbdd3a1fa99c1efe4
jyonitkapuriya <kapuriyajyonit755@gmail.com>
2024-03-16 17:43:01 +0530

    Remove all code

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\SM.PC\\Downloads\\nextjs-tailwind-portfolio-page-main\\nextjs-tailwind-portfolio-page-main\\src\\app\\layout.tsx": [
+      "C:\\Users\\SM.PC\\Downloads\\nextjs-tailwind-portfolio-page-main\\nextjs-tailwind-portfolio-page-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"300\",\"400\",\"500\",\"700\",\"900\"],\"display\":\"swap\"}],\"variableName\":\"roboto\"}",
+      "C:\\Users\\SM.PC\\Downloads\\nextjs-tailwind-portfolio-page-main\\nextjs-tailwind-portfolio-page-main\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\SM.PC\\Downloads\\nextjs-tailwind-portfolio-page-main\\nextjs-tailwind-portfolio-page-main\\src\\app\\page": [
+      "C:\\Users\\SM.PC\\Downloads\\nextjs-tailwind-portfolio-page-main\\nextjs-tailwind-portfolio-page-main\\src\\app\\globals.css",
+      "C:\\Users\\SM.PC\\Downloads\\nextjs-tailwind-portfolio-page-main\\nextjs-tailwind-portfolio-page-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"300\",\"400\",\"500\",\"700\",\"900\"],\"display\":\"swap\"}],\"variableName\":\"roboto\"}"
+    ]
+  }
+}
